76f286bf5167c3bcbcc941bf1f9867280d05518b
farzam7777 <l144211@lhr.nu.edu.pk>
2018-04-05 00:50:28 +0500

    heroku gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
 
-	attribute :picture_url, :text
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :omniauthable,
