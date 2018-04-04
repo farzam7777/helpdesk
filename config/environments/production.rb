54da6c5aeefcfdca80a2cc5e97ba199c148d6fc6
farzam7777 <l144211@lhr.nu.edu.pk>
2018-04-05 02:38:58 +0500

    google auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,15 +4,15 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # config.action_mailer.default_url_options = { :host => 'https://www.araddi.com/' }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #     :address              => "smtp.gmail.com",
-  #     :port                 => 587,
-  #     :user_name            => 'araddi.com@gmail.com',
-  #     :password             => 'Waloyasecurenew1101!',
-  #     :authentication       => 'plain',
-  #     :enable_starttls_auto => true  }
+  config.action_mailer.default_url_options = { :host => 'https://helpdesk786.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'l144211@lhr.nu.edu.pk',
+      :password             => 'bismillah786?',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -25,7 +25,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  
+
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
