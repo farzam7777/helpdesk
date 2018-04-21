b18303b786ed0c7d2b31ab2856d3f1faddffff31
farzam7777 <l144211@lhr.nu.edu.pk>
2018-04-21 20:32:08 +0500

    back end set up

--- app/admin/category_users.rb
+++ app/admin/category_users.rb
@@ -1,15 +1,12 @@
 ActiveAdmin.register CategoryUser do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+	permit_params :category_id, :user_id, :permission
 
+	form multipart: true do |f|
+	  f.inputs "Category User Details" do
+	  	f.input :category
+	  	f.input :user																																																																																																																																																																																																																																																																																																																																																			
+	  	f.input :permission, as: :select, collection: ['Manage', 'Read']
+	  end
+	  f.button :Submit
+	end
 end
--- app/admin/services.rb
+++ app/admin/services.rb
@@ -1,15 +1,28 @@
 ActiveAdmin.register Service do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+	permit_params :kind, :description, :pdf_needed, :category_id, :status, :title
 
+	index do
+	  selectable_column
+	  column :title
+	  column :kind
+	  column 'Description' do |service|
+      truncate(service.description, length: 15)
+    end
+	  column :pdf_needed, as: :boolean
+	  column :category
+	  column :status																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																												
+	  actions
+	end
+
+	form multipart: true do |f|
+	  f.inputs "Service Details" do
+	  	f.input :category
+	  	f.input :title																																																																																																																																																																																																																																																																																																																																																			
+	  	f.input :kind
+	  	f.input :pdf_needed, as: :select, collection: [['Yes', 1], ['No', 0]]
+	  	f.input :status, as: :select, collection: [['Public', 'public'], ['Private', 'private']]
+	  	f.input :description
+	  end
+	  f.button :Submit
+	end
 end
